d49ccca078b66c7a9ad8413a31b073c4c54317b0
KratiGupta1224 <kratigupta1224@gmail.com>
2025-04-26 20:38:55 +0530

    Third commit

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ const faqItems = document.querySelectorAll('.faq-item');
       behavior: 'smooth'
     });
   }
+
+ 
+  const menuBtn = document.getElementById('menu-btn');
+  const menu = document.getElementById('menu');
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+
